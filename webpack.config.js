cc64555c4cb19b60dec8d6761ec3c9020f43bb81
白家玮 <2443891115@qq.com>
2021-01-20 21:59:06 +0800

    webpack 配置完成

--- webpack.config.js
+++ webpack.config.js
@@ -1,48 +1,23 @@
-const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin')
-module.exports={
-    mode:'production',
-    entry:{
-        index:'./lib/index.tsx'
+const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+module.exports = {
+    entry: {
+        index: './lib/index.tsx'
     },
-    resolve:{
-        extensions:['.ts','.js','.tsx','jsx']
+    resolve: {
+        extensions: ['.ts', '.tsx', '.js', '.jsx'],
     },
-    // 输出
-    output:{
+    output: {
         path: path.resolve(__dirname, 'dist/lib'),
-        library:'react',
-        libraryTarget:'umd',
-    
-      
+        library: 'REACT-ui',
+        libraryTarget: 'umd',
     },
-    // 输入tsx 
-    module:{
-        rules:[
+    module: {
+        rules: [
             {
-                test:/\.tsx?$/,
-                loader:'awesome-typescript-loader'
+                test: /\.tsx?$/,
+                loader: 'awesome-typescript-loader'
             }
         ]
     },
-    // plugins:[
-    //     new HtmlWebpackPlugin({
-    //         title:'GUU',
-    //         template:'index.html'
-    //     })
-    // ],
-    
-    // externals:{
-    //     react:{
-    //         commonjs:'react',
-    //         commonjs2:'react',
-    //         amd:'react',root:'React',
-            
-    //     },
-    //     'react-dom':{
-    //         commonjs:'react-dom',
-    //         commonjs2:'react-dom',
-    //         amd:'react-dom',root:'ReactDom',
-    //     }
-    // }
 }
