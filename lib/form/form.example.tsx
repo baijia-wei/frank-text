5e1a2439a1a58233a61000aa87079aedcb6bf257
白家玮 <2443891115@qq.com>
2021-05-14 11:07:58 +0800

    表单完成

--- lib/form/form.example.tsx
+++ lib/form/form.example.tsx
@@ -2,45 +2,82 @@ import React, {useState ,Fragment} from "react";
 import Form,{FormValue} from "./form";
 import Validator, {noError} from './validator';
 import Buttonss from '../button/button';
+
+
+const usernames = ['frank', 'jack', 'frankfrank', 'alice', 'bob'];
+const checkUserName = (username: string, succeed: () => void, fail: () => void) => {
+    setTimeout(() => {
+        console.log('我现在知道用户名是否存在');
+        if (usernames.indexOf(username) >= 0) {
+            fail();
+        } else {
+            succeed();
+        }
+    }, 2000);
+};
+
 const FormExample: React.FunctionComponent = () =>{
-    const  [formData,setFromData]=useState<FormValue>({
-        username:'',
-        password:''
+    const [formData, setFormData] = useState<FormValue>({
+        username: '',
+        password: ''
     });
-
     const [fields]=useState([
-        {name:'username',label:'用户名',input:{type:'text'}},
-        {name:'password',label:'密码',input: {type: 'password'}}
+
+        {name: 'image', label: '头像', input: {type: 'text'}},
+        {name: 'password', label: '密码', input: {type: 'password'}},
     ]);
-    const [errors,setErors]=useState({});
-    const onSubmit=(e:React.FormEvent<HTMLFormElement>)=>{
+    const [errors, setErrors] = useState({});
+    const validator = (username: string) => {
+        return new Promise<string>((resolve, reject) => {
+            checkUserName(username, resolve, () => reject('unique'));
+        });
+    };
+    const onSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+        const rules = [
+            {key: 'username', required: true},
+            {key: 'username', minLength: 8, maxLength: 16},
+            {key: 'username', validator},
+            {key: 'username', validator},
+            {key: 'username', pattern: /^[A-Za-z0-9]+$/},
+            {key: 'password', required: true},
+            {key: 'password', validator},
+            {key: 'password', validator},
+        ];
+        Validator(formData, rules, (errors) => {
+            console.log(errors);
+            setErrors(errors);
+            if (noError(errors)) {
+                // 没错
+            }
+        });
+    };
+    const transformError = (message: string) => {
+        const map: any = {
+            unique: 'username is taken',
+            required: 'required',
+            minLength: 'too short',
+            maxLength: 'too long',
+        };
+        return map[message];
+    };
 
-        const  rules=[
-            {key:'username',required:true},
-            {key:'username',minLength:8,maxLength:11},
-            {key:'username',pattern:/^[A-Za-z0-9]+$/},
-            {key:'password',required:true},
-        ]
-        Validator(formData,rules)
-        setErors(errors)
-
-        if (noError(errors)) {
-
-        }
-    }
-    return(
-        <Form value={formData} fields={fields}
-        buttons={
-            <Fragment>
-                <Buttonss type="submit">提交</Buttonss>
-                <Buttonss>返回</Buttonss>
-            </Fragment>
-        }
-              errors={errors}
-              onChange={(newValue)=>setFromData(newValue)}
-              onSubmit={onSubmit}
-
-        />
+    return (
+        <div>
+            <Form value={formData}
+                  fields={fields}
+                  buttons={
+                      <Fragment>
+                          <Buttonss type="submit" level="important">提交</Buttonss>
+                          <Buttonss>返回</Buttonss>
+                      </Fragment>
+                  }
+                  errors={errors}
+                  transformError={transformError}
+                  onChange={(newValue) => setFormData(newValue)}
+                  onSubmit={onSubmit}
+            />
+        </div>
     );
 };
-export  default  FormExample
+
+export default FormExample;
--- lib/form/form.tsx
+++ lib/form/form.tsx
@@ -1,10 +1,18 @@
-import React, {ReactFragment} from "react";
-import Input from "../input/input";
-import classes from "../helpers/classes";
+
+/*
+ * TODO:
+ * 1. 支持子字段编辑（子表单）
+ * 2. 支持更多的 type / 自定义的 input
+ * 3. 支持手机端
+ */
+import * as React from 'react';
+import {ReactFragment} from 'react';
+import Input from '../input/input';
+import classes from '../helpers/classes';
 import './form.scss';
 
 export interface FormValue {
-    [T: string]: any
+    [K: string]: any
 }
 
 interface Props {
@@ -13,21 +21,31 @@ interface Props {
     buttons: ReactFragment;
     onSubmit: React.FormEventHandler<HTMLFormElement>;
     onChange: (value: FormValue) => void;
-    errors: { [k: string]: string[] }
+    errors: { [K: string]: string[] };
+    errorsDisplayMode?: 'first' | 'all';
+    transformError?: (message: string) => string;
 }
 
 const Form: React.FunctionComponent<Props> = (props) => {
-    const formData = props.value
-    const onsubmit: React.FormEventHandler<HTMLFormElement> = (e) => {
-        e.preventDefault()
-        props.onSubmit(e)
-    }
+    const formData = props.value;
+    const onSubmit: React.FormEventHandler<HTMLFormElement> = (e) => {
+        e.preventDefault();
+        props.onSubmit(e);
+    };
     const onInputChange = (name: string, value: string) => {
         const newFormValue = {...formData, [name]: value};
-        props.onChange(newFormValue)
-    }
+        props.onChange(newFormValue);
+    };
+    const transformError = (message: string) => {
+        const map: any = {
+            required: '必填',
+            minLength: '太短',
+            maxLength: '太长',
+        };
+        return props.transformError && props.transformError(message) || map[message] || '未知错误';
+    };
     return (
-        <form onSubmit={onsubmit}>
+        <form onSubmit={onSubmit}>
             <table className="fui-form-table">
                 <tbody>
                 {props.fields.map(f =>
@@ -36,11 +54,17 @@ const Form: React.FunctionComponent<Props> = (props) => {
                             <span className="fui-form-label">{f.label}</span>
                         </td>
                         <td className="fui-form-td">
-                            <Input className="fui-form-input" type={f.input.type} value={formData[f.name]}
-                                   onChange={(e) => onInputChange(f.name, e.target.value)}/>
-                            <div>
-                                {props.errors[f.name]}
-                            </div>
+                            <Input className="fui-form-input"
+                                   type={f.input.type}
+                                   value={formData[f.name]||""}
+                                   onChange={(e) => onInputChange(f.name, e.target.value)}
+                            />
+                            <div className="fui-form-error">{
+                                props.errors[f.name] ?
+                                    (props.errorsDisplayMode === 'first' ?
+                                        transformError!(props.errors[f.name][0]) : props.errors[f.name].map(transformError!).join()) :
+                                    <span>&nbsp;</span>
+                            } </div>
                         </td>
                     </tr>
                 )}
@@ -55,4 +79,9 @@ const Form: React.FunctionComponent<Props> = (props) => {
         </form>
     );
 };
-export default Form
+
+Form.defaultProps = {
+    errorsDisplayMode: 'first',
+};
+
+export default Form;
