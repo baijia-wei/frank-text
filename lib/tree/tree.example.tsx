40c380aa6df94fa86bc23072046ef9bdde584934
baijiawei <baijiawei@1551380035528060.onaliyun.com>
2021-09-29 19:28:14 +0800

    升级TS 递归渲染 树形组件

--- lib/tree/tree.example.tsx
+++ lib/tree/tree.example.tsx
@@ -4,9 +4,9 @@ const TreeExample: React.FunctionComponent = () => {
     const [array, setArray] = useState([{
         text: '一职中',
         value: "1",
-        childern: [
+        children: [
             {
-                text: "一直用1.1", value: "1.1", childern: [
+                text: "一直用1.1", value: "1.1", children: [
                     { text: "一直用1.1.1", value: "1.1.1" },
                     { text: "一直用1.1.2", value: '1.1.2' },
                     { text: "一直用1.1.3", value: '1.1.3' },
@@ -16,12 +16,12 @@ const TreeExample: React.FunctionComponent = () => {
             { text: "一直用1.2", value: '1.2' },
             { text: "一直用1.3", value: '1.3' },
             {
-                text: "一直用1.4", value: '1.4', childern: [
+                text: "一直用1.4", value: '1.4', children: [
                     { text: "一直用1.4.1", value: "1.4.1" },
                     { text: "一直用1.4.2", value: '1.4.2' },
                     { text: "一直用1.4.3", value: '1.4.3' },
                     {
-                        text: "一直用1.4.4", value: '1.4.4', childern: [
+                        text: "一直用1.4.4", value: '1.4.4', children: [
                             { text: "一直用1.4.4.1", value: "1.4.4.1" },
                             { text: "一直用1.4.4.2", value: '1.4.4.2' },
                             { text: "一直用1.4.4.3", value: '1.4.4.3' },
@@ -36,11 +36,11 @@ const TreeExample: React.FunctionComponent = () => {
     {
         text: '二职中',
         value: "2",
-        childern: [
+        children: [
             {
-                text: "二直用1", value: "2.1", childern: [
+                text: "二直用1", value: "2.1", children: [
                     {
-                        text: "二直用2.1.1", value: "2.1.1", childern: [
+                        text: "二直用2.1.1", value: "2.1.1", children: [
                             { text: "二直用2.1.1", value: "2.1.1" },
 
                         ]
@@ -48,18 +48,18 @@ const TreeExample: React.FunctionComponent = () => {
                 ]
             },
             {
-                text: "二直用2", value: '2.2', childern: [
+                text: "二直用2", value: '2.2', children: [
                     { text: "二直用2.2.1", value: "2.2.1" },
 
                 ]
             },
             {
-                text: "二直用3", value: '2.3', childern: [
+                text: "二直用3", value: '2.3', children: [
                     { text: "二直用2.3.1", value: "2.3.1" },
 
                 ]
             },
-            { text: "二直用4", value: '2.4' , childern: [
+            { text: "二直用4", value: '2.4' , children: [
                 { text: "二直用2.4.1", value: "2.4.1" },
 
             ]},
--- lib/tree/tree.tsx
+++ lib/tree/tree.tsx
@@ -1,26 +1,30 @@
 import React from "react";
 
-interface SourceDataItem{
-    text:string;
-    value:string;
-    children?:SourceDataItem[]
+interface SourceDataItem {
+    text: string;
+    value: string;
+    children?: SourceDataItem[]
 }
-interface Props{
-    sourceData:SourceDataItem[]
+interface Props {
+    sourceData: SourceDataItem[]
 }
-const Tree:React.FunctionComponent<Props>=(Props)=>{
+
+const x=(item:SourceDataItem)=>{
+    return <div key={item.value}>
+        {item.text}
+        {item.children?.map(sub=>{
+            return x(sub)
+        })}
+    </div>
+}
+const Tree: React.FunctionComponent<Props> = (props) => {
     return (
         <div>
-           {/* {Props.sourceData[0].text} */}
-           {
-               Props.sourceData.map((item,index)=>{
-                return <div key={index}> {item.text}
-                        {item.children?.map(item2=>{
-                            return <div> { item2.text}</div>
-                        })}
-                </div>
-               })
-           }
+            {/* {Props.sourceData[0].text} */}
+            {
+                props.sourceData?.map((item, index) => {
+                    return x(item)
+                })}
         </div>
     );
 };
