2416b0bb5aaa8b05c3d59533ffcca7557c62a32f
白家玮 <2443891115@qq.com>
2022-03-11 20:00:46 +0800

    城市完成

--- lib/citySelect/citySelect.exampie.tsx
+++ lib/citySelect/citySelect.exampie.tsx
@@ -6,14 +6,16 @@ interface Props {
 
 }
 const CitySelectExample: FunctionComponent<Props> = (props) => {
-
-
+    const onChangex = (e:any) => {
+        console.log(e);
+        
+    }
+    const citylist = ['鞍山',"深圳" ,"中山","安庆" ,"琴岛","安平", "安宁", "安福", '桂林', "盘锦市", "上海", "忻州市", "汾阳市", "河津市", "孝义市"]
     return (
         <div>
             <div><h2>第一个例子</h2>
-                <CitySelect>选择城市</CitySelect>
+                <CitySelect onChange={onChangex} dataSource={citylist}>选择城市</CitySelect>
             </div>
-
         </div>
     )
 }
--- lib/citySelect/citySelect.tsx
+++ lib/citySelect/citySelect.tsx
@@ -1,56 +1,98 @@
-import React, { FunctionComponent, useEffect, useState } from "react";
+import React, { Dispatch, SetStateAction, FunctionComponent, useContext, useEffect, useState } from "react";
 import ReactDOM from "react-dom";
+import pinyin from "tiny-pinyin"
 import "./citySelect.scss"
 interface Props {
-
+    dataSource: string[];
+    onChange: (p1: string) => void;
 }
+interface Context {
+    map: { [key: string]: string[] };
+    onChange: (p1: string) => void;
+    setDialogVisible: Dispatch<SetStateAction<boolean>>;
+}
+const CitySelectContext = React.createContext<Context>({
+    map: {}, onChange: (p1: string) => { }, setDialogVisible: () => { }
+});
 const CitySelect: FunctionComponent<Props> = (props) => {
-    const [dialogVisible, setDialogVisble] = useState<Boolean>(false)
+    const [dialogVisible, setDialogVisible] = useState<Boolean>(false)
+    const map: { [key: string]: string[] } = {}
+    props.dataSource.map((city) => {
+        const py = pinyin.convertToPinyin(city)
+        const index = py[0]
+        map[index] = map[index] || []
+        map[index].push(city)
+
+    })
+
     const onClick = () => {
-        setDialogVisble(true)
+        setDialogVisible(true)
+
     }
     return (
-        <div>
+        <CitySelectContext.Provider value={{ map, onChange: props.onChange, setDialogVisible }}>
             <div onClick={onClick}>{props.children}</div>
-            {dialogVisible && <Dialog onClose={() => { setDialogVisble(false) }}></Dialog>}
-
-        </div>
+            {dialogVisible && <Dialog onClose={() => setDialogVisible(false)} />}
+        </CitySelectContext.Provider>
     )
 }
 export default CitySelect
 const Dialog: React.FunctionComponent<{ onClose: () => void }> = (props) => {
+    const { map, onChange } = useContext(CitySelectContext);
+    const indexList = Object.keys(map).sort()
+    const cityList = (Object.entries(map)
+        .sort((a, b) => a[0].charCodeAt(0) - b[0].charCodeAt(0)));
+    const onClick = (city: string) => {
+        onChange(city);
+    };
     return ReactDOM.createPortal((
         <div className='fui-citySelect-dialog' onClick={props.onClose}>
-            <header > 
+            <header >
                 <span className="icon">&lt;</span>
                 <span>选择城市</span>
             </header>
 
             <CurrentLocation></CurrentLocation>
             <h2>全部城市</h2>
-            <div className='cityIndex'>Abcd</div>
-            <div className='cityList'>所有城市</div>
-        </div>
+            <ol className="fui-citySelect-index">
+                {indexList.map(a => <li key={a}>{a}</li>)}
+            </ol>
+            <div className="cityList">所有城市</div>
+            
+            {cityList.map(([letter, list]) => {
+                return (
+                    <div key={letter} className="fui-citySelect-citySection">
+                        <h4 data-letter={letter}>{letter}</h4>
+                        {list.map(city =>
+                            <div className="fui-citySelect-cityName" key={city}
+                                onClick={() => onClick(city)}
+                            >{city}</div>
+                        )}
+                    </div>
+                );
+            })}
+
+        </div >
     ), document.body)
 
 }
 
 const CurrentLocation: React.FunctionComponent = () => {
     const [city, setCity] = useState<string>("加载中...")
-    useEffect(()=>{
-        const xhr =new XMLHttpRequest()
-        xhr.open( "get",'http://ip-api.com/json/?lang=zh-CN')
-        xhr.onload=()=>{
-            const string =xhr.responseText;
-            const obj =JSON.parse(string);
-            const c=obj.city;
+    useEffect(() => {
+        const xhr = new XMLHttpRequest()
+        xhr.open("get", 'http://ip-api.com/json/?lang=zh-CN')
+        xhr.onload = () => {
+            const string = xhr.responseText;
+            const obj = JSON.parse(string);
+            const c = obj.city;
             setCity(c)
         };
-        xhr.onerror=()=>{
+        xhr.onerror = () => {
             setCity('未知')
         }
         xhr.send();
-    },[])
+    }, [])
     return (
         <div className="currentCity">
             当前城市:{city}
