7e634177a9c5b57d7828b7e961c1b0362e06a7d1
baijw <2443891115@qq.com>
2023-02-28 12:15:46 +0800

    版本升级

--- example.tsx
+++ example.tsx
@@ -1,6 +1,6 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
-import { HashRouter as Router, Route, NavLink } from 'react-router-dom';
+import { HashRouter as Router, Route, NavLink, Routes } from 'react-router-dom';
 import DialogExample from './lib/dialog/dialog.example';
 import ButtonExample from './lib/button/button.example';
 import LayoutExample from './lib/layout/layout.example';
@@ -31,158 +31,142 @@ import SwitchExample from "./lib/switch/switch.example";
 import { Icon } from './lib';
 const git = require('./lib/icons/GitHub.png');
 const log = require("./lib/assets/logo.png");
-const kj = require("./lib/assets/kj.svg");
+import {
+    createRoot
+} from 'react-dom/client';
 
 
 
-const openModal = () => {
-    const close = modal(
-        <div className="ands-head">
-            <div className="ands-manchu">
-
+const container = document.querySelector('#root');
+const root = createRoot(container);
+root.render(<Router>
+    <Layout className="site-page">
+        <Header className="site-header">
+            <div className="logo">
+                <img src={log} width="48" height="48" alt="logo" />
+                <span>Ands</span>
             </div>
-            <div>
+            <div className="git">
+                <a href="https://github.com/baijia-wei/frank-text">
+                    <img src={git} width="20" height="20" alt="github" />
+                    GitHub
+                </a>
             </div>
-            <div className="ands-lanzhou-img">
-                <div>
-                    <img src="lib/icons/wx.png" alt="微信" />
-                    <img src="lib/icons/zfb.png" alt="支付宝" />
-                </div>
+        </Header>
 
-            </div>
-            <Buttonss onClick={() => close()}>关闭</Buttonss>
-        </div>);
-};
+        <Layout>
+            <Aside className="site-aside">
+                <ul>
+                    <li>
+                        <NavLink to="/ganging">安装</NavLink>
+                    </li>
+                    <li>
+                        <NavLink to="/updated">更新日志</NavLink>
+                    </li>
+                    <li className="site-aside-lanzhou">
 
-ReactDOM.render(
-    <Router>
-        <Layout className="site-page">
-            <Header className="site-header">
-                <div className="logo">
-                    <img src={log} width="48" height="48" alt="logo" />
-                    <span>Ands</span>
-                </div>
-                <div className="git">
-                    <a href="https://github.com/baijia-wei/frank-text">
-                        <img src={git} width="20" height="20" alt="github" />
-                        GitHub
-                    </a>
-                </div>
-            </Header>
+                    </li>
+                </ul>
+                <h2>组件</h2>
+                <ul>
+                    <li>
+                        <NavLink to="/icon">Icon</NavLink>
+                    </li>
+                    <li>
+                        <NavLink to="/button">Button 按钮</NavLink>
+                    </li>
+                    <li>
+                        <NavLink to="/dialog">对话框</NavLink>
+                    </li>
+                    <li>
+                        <NavLink to="/layout">布局</NavLink>
+                    </li>
+                    <li>
+                        <NavLink to="/form">表单</NavLink>
+                    </li>
 
-            <Layout>
-                <Aside className="site-aside">
-                    <ul>
-                        <li>
-                            <NavLink to="/ganging">安装</NavLink>
-                        </li>
-                        <li>
-                            <NavLink to="/updated">更新日志</NavLink>
-                        </li>
-                        <li className="site-aside-lanzhou">
+                    <li>
+                        <NavLink to="/tree">树形组件</NavLink>
+                    </li>
 
-                        </li>
-                    </ul>
-                    <h2>组件</h2>
-                    <ul>
-                        <li>
-                            <NavLink to="/icon">Icon</NavLink>
-                        </li>
-                        <li>
-                            <NavLink to="/button">Button 按钮</NavLink>
-                        </li>
-                        <li>
-                            <NavLink to="/dialog">对话框</NavLink>
-                        </li>
-                        <li>
-                            <NavLink to="/layout">布局</NavLink>
-                        </li>
-                        <li>
-                            <NavLink to="/form">表单</NavLink>
-                        </li>
+                    <li>
+                        <NavLink to="/input">输入框</NavLink>
+                    </li>
+                    <li>
+                        <NavLink to="/scroll">滚动条</NavLink>
+                    </li>
+                    <li>
+                        <NavLink to="/choice">选择器</NavLink>
+                    </li>
 
-                        <li>
-                            <NavLink to="/tree">树形组件</NavLink>
-                        </li>
+                    <li>
+                        <NavLink to="/hoc">草稿练习</NavLink>
+                    </li>
+                    <li>
+                        <NavLink to="/dragpull">拖拽</NavLink>
+                    </li>
+                    <li>
+                        <NavLink to="/cityselect">城市组件</NavLink>
+                    </li>
 
-                        <li>
-                            <NavLink to="/input">输入框</NavLink>
-                        </li>
-                        <li>
-                            <NavLink to="/scroll">滚动条</NavLink>
-                        </li>
-                        <li>
-                            <NavLink to="/choice">选择器</NavLink>
-                        </li>
+                    <li>
+                        <NavLink to="/actionsheet">动作面板</NavLink>
+                    </li>
+                    <li>
+                        <NavLink to="/tabexample">Tabs</NavLink>
+                    </li>
+                    <li>
+                        <NavLink to="/tables">Tables 表格</NavLink>
+                    </li>
 
-                        <li>
-                            <NavLink to="/hoc">草稿练习</NavLink>
-                        </li>
-                        <li>
-                            <NavLink to="/dragpull">拖拽</NavLink>
-                        </li>
-                        <li>
-                            <NavLink to="/cityselect">城市组件</NavLink>
-                        </li>
+                    <li>
+                        <NavLink to="/redux">Redux</NavLink>
+                    </li>
+                    <li>
+                        <NavLink to="/radio">Radio</NavLink>
+                    </li>
 
-                        <li>
-                            <NavLink to="/actionsheet">动作面板</NavLink>
-                        </li>
-                        <li>
-                            <NavLink to="/tabexample">Tabs</NavLink>
-                        </li>
-                        <li>
-                            <NavLink to="/tables">Tables 表格</NavLink>
-                        </li>
+                    <li>
+                        <NavLink to="/switch">Switch</NavLink>
+                    </li>
+                    <li>
+                        <NavLink to="/activity">活动</NavLink>
+                    </li>
+                    <li>
+                        <NavLink to="/longList">Long list</NavLink>
+                    </li>
 
-                        <li>
-                            <NavLink to="/redux">Redux</NavLink>
-                        </li>
-                        <li>
-                            <NavLink to="/radio">Radio</NavLink>
-                        </li>
+                </ul>
+            </Aside>
+            <Content>
+                <Routes>
+                    <Route path="/icon" element={<IconDemo />} />
+                    <Route path="/button" element={<ButtonExample />} />
+                    <Route path="/dialog" element={<DialogExample />} />
+                    <Route path="/layout" element={<LayoutExample />} />
+                    <Route path="/ganging" element={<GanYang />} />
+                    <Route path="/updated" element={<UpdateLog />} />
+                    <Route path="/input" element={<InputExample />} />
+                    {/* <Route path="/hoc"  element={<Hoc name={''} stage={''}/>}/> */}
+                    <Route path="/tree" element={<TreeExample />} />
+                    <Route path="/scroll" element={<Scroll />} />
+                    <Route path="/choice" element={<MultipleChoice />} />
+                    {/* <Route path="/dragpull"  element={<dragPull/>}  />
+                <Route path="/cityselect" element={<cityselect/>}  />
+                <Route path="/actionsheet"  element={<actionsheet/>} /> */}
+                    <Route path="/tabexample" element={<TabExample />} />
+                    <Route path="/tables" element={<TablesExample />} />
+                    <Route path="/redux" element={<ReduxExample />} />
+                    <Route path="/Radio" element={<RadioExample />} />
+                    <Route path="/switch" element={<SwitchExample />} />
+                    <Route path="/activity" element={<ActivityExample />} />
+                    {/* <Route path="/longList"   element={<longList/>} /> */}
+                </Routes>
 
-                        <li>
-                            <NavLink to="/switch">Switch</NavLink>
-                        </li>
-                        <li>
-                            <NavLink to="/activity">活动</NavLink>
-                        </li>
-                        <li>
-                            <NavLink to="/longList">Long list</NavLink>
-                        </li>
-
-                    </ul>
-                </Aside>
-                <Content>
-                    <Route path="/icon" component={IconDemo} />
-                    <Route path="/button" component={ButtonExample} />
-                    <Route path="/dialog" component={DialogExample} />
-                    <Route path="/layout" component={LayoutExample} />
-                    <Route path="/ganging" component={GanYang} />
-                    <Route path="/updated" component={UpdateLog} />
-                    <Route path="/form" component={FormExample} />
-                    <Route path="/input" component={InputExample} />
-                    <Route path="/hoc" component={Hoc} />
-                    <Route path="/tree" component={TreeExample} />
-                    <Route path="/scroll" component={Scroll} />
-                    <Route path="/choice" component={MultipleChoice} />
-                    <Route path="/dragpull" component={dragPull} />
-                    <Route path="/cityselect" component={cityselect} />
-                    <Route path="/actionsheet" component={actionsheet} />
-                    <Route path="/tabexample" component={TabExample} />
-                    <Route path="/tables" component={TablesExample} />
-                    <Route path="/redux" component={ReduxExample} />
-                    <Route path="/Radio" component={RadioExample} />
-                    <Route path="/switch" component={SwitchExample} />
-                    <Route path="/activity" component={ActivityExample} />
-                    <Route path="/longList" component={longList} />
-
-                </Content>
-            </Layout>
-            <Footer className="site-footer">
-                &copy; 白家玮
-            </Footer>
+            </Content>
         </Layout>
-    </Router >
-    , document.querySelector('#root'));
+        <Footer className="site-footer">
+            &copy; 白家玮
+        </Footer>
+    </Layout>
+</Router >);
